60fc1291bb8588f549449a857b4fb2a3ff070d80
Lydia Lei <llei194@student.fuhsd.org>
2024-01-05 01:12:54 -0800

    implement related products

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -24,44 +24,44 @@ export const ProductHero: React.FC<{
 
   return (
     <Gutter className={classes.productHero}>
-      <div className={classes.categories}>
-        {categories?.map((category, index) => {
-          const { title: categoryTitle } = category
-
-          const titleToUse = categoryTitle || 'Untitled category'
-
-          const isLast = index === categories.length - 1
-
-          return (
-            <Fragment key={index}>
-              {titleToUse}
-              {!isLast && <Fragment>, &nbsp;</Fragment>}
-            </Fragment>
-          )
-        })}
-      </div>
-      <h1 className={classes.title}>{title}</h1>
-      <div>
-        <p className={classes.description}>
-          {`${description ? `${description} ` : ''}To edit this product, `}
-          <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/{id`}>
-          navigate to the admin dashboard
-          </Link>
-          {'.'}
-        </p>
-      </div>
-      <Price product={product} button={false} />
-      <AddToCartButton product={product} className={classes.addToCartButton} />
-      <div className={classes.media}>
-        <div className={classes.mediaWrapper}>
-          {!metaImage && <div className={classes.placeholder}>No image</div>}
-          {metaImage && typeof metaImage !== 'string' && (
-            <Media imgClassName={classes.image} resource={metaImage} fill />
-          )}
-        </div>
-        {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-          <RichText content={metaImage.caption} className={classes.caption} />
+      <div className={classes.mediaWrapper}>
+        {!metaImage && <div className={classes.placeholder}>No image</div>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media imgClassName={classes.image} resource={metaImage} fill />
         )}
+      </div>
+
+      <div className={classes.details}>
+        <h3 className={classes.title}>{title}</h3>
+
+        <div className={classes.categoryWrapper}>
+          <div className={classes.categories}>
+            {categories?.map((category, index) => {
+              const { title: categoryTitle } = category
+
+              const titleToUse = categoryTitle || 'Untitled category'
+
+              const isLast = index === categories.length - 1
+
+              return (
+                <p key={index} className={classes.category}>
+                  {titleToUse} {!isLast && <Fragment>, &nbsp;</Fragment>} 
+                  <span className={classes.separator}>|</span>
+                </p>
+              )
+            })}
+          </div>
+          <p className={classes.stock}> In stock</p>
+        </div>
+      
+        <Price product={product} button={false} />
+
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
+        </div>
+        
+        <AddToCartButton product={product} className={classes.addToCartButton} />
       </div>
     </Gutter>
   )
